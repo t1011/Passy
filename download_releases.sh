cb2c41e600b825b4c127497d27aa509c0df8be3a
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2026-03-23 23:14:01 +0000

    Update download_releases.sh

--- download_releases.sh
+++ download_releases.sh
@@ -29,35 +29,45 @@ mkdir -p ./build/gh_releases
 cd ./build/gh_releases
 
 echo 'Downloading latest release artifacts...'
-gh run download \
-  -n android \
-  -n cli-linux \
-  -n cli-linux-arm64 \
-  -n cli-linux-armv7 \
-  -n linux-appimage \
-  -n linux-arm64-appimage \
-  -n linux-arm64-bundle \
-  -n linux-arm64-bundle-no-updates-popup \
-  -n linux-bundle \
-  -n linux-bundle-no-updates-popup \
-  -n linux-snap \
-  -n linux-snap-arm64 \
-  -n windows-exe-installer \
-  -n windows-portable
+echo '[1/10] android...'
+gh run download -n android
+sleep 5
+echo '[2/10] cli-linux...'
+gh run download -n cli-linux
+sleep 5
+echo '[3/10] cli-linux-arm...'
+gh run download -n cli-linux-arm64 -n cli-linux-armv7
+sleep 5
+echo '[4/10] linux-appimage...'
+gh run download -n linux-appimage
+sleep 5
+echo '[5/10] linux-arm64-appimage...'
+gh run download -n linux-arm64-appimage
+sleep 5
+echo '[6/10] linux-arm64-bundle...'
+gh run download -n linux-arm64-bundle -n linux-arm64-bundle-no-updates-popup
+sleep 5
+echo '[7/10] linux-bundle...'
+gh run download -n linux-bundle -n linux-bundle-no-updates-popup
+sleep 5
+echo '[8/10] linux-snap...'
+gh run download -n linux-snap
+sleep 5
+echo '[9/10] linux-snap-arm64...'
+gh run download -n linux-snap-arm64
+sleep 5
+echo '[10/10] windows...'
+gh run download -n windows-exe-installer -n windows-portable
 echo 'Done.'
 
-echo 'Preparing Passy Snap...'
-cp linux-snap/* .
-echo 'Preparing Passy Snap ARM64...'
-cp linux-snap-arm64/* .
 echo 'Preparing Passy CLI Linux...'
-cp cli-linux/Passy-CLI-Linux.zip ./Passy-CLI-v$version-Linux.zip
+cp Passy-CLI-Linux.zip ./Passy-CLI-v$version-Linux.zip
 echo 'Preparing Passy CLI Linux ARM64...'
 cp cli-linux-arm64/Passy-CLI-Linux-ARM64.zip ./Passy-CLI-v$version-Linux-ARM64.zip
 echo 'Preparing Passy CLI Linux ARMv7...'
 cp cli-linux-armv7/Passy-CLI-Linux-ARMv7.zip ./Passy-CLI-v$version-Linux-ARMv7.zip
 echo 'Preparing Passy Android Apk...'
-cp android/Passy-Android.apk ./Passy-v$version.apk
+cp Passy-Android.apk ./Passy-v$version.apk
 zip -9 ./Passy-v$version-Android-Apk.zip Passy-v$version.apk 
 rm Passy-v$version.apk
 echo 'Preparing Passy Linux ARM64...'
@@ -74,13 +84,11 @@ cd windows-portable
 zip -9 -r ../Passy-v$version-Windows-Portable.zip Passy
 cd ..
 echo 'Preparing Passy Linux AppImage...'
-cp linux-appimage/Passy-Linux-AppImage.zip .
 unzip Passy-Linux-AppImage.zip
 rm Passy-Linux-AppImage.zip
 chmod +x Passy-Latest-x86_64.AppImage
 mv Passy-Latest-x86_64.AppImage Passy-v$version-x86-64.AppImage
 echo 'Preparing Passy Linux ARM64 AppImage...'
-cp linux-arm64-appimage/Passy-Linux-ARM64-AppImage.zip .
 unzip Passy-Linux-ARM64-AppImage.zip
 rm Passy-Linux-ARM64-AppImage.zip
 chmod +x Passy-Latest-aarch64.AppImage
